e1f2a9118f6e26ccdfa217a7082ecc256b593e9a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2015-11-30 20:00:27 +0100

    ADD: new routes

--- js/app.js
+++ js/app.js
@@ -21,16 +21,32 @@ app.config(function($routeProvider, $locationProvider){
 			controller: 'cartController',
 			templateUrl: SETTINGS.views() + 'cart.html',
 			resolve: {
-				isLoggedIn: checkLogin,
-				exists : checkPage
+				isLoggedIn: checkLogin
+				//exists : checkPage
 			}
 		})
-		.when('/:category', {
-			controller: 'categoryController',
-			templateUrl: SETTINGS.views() + 'category.html',
+		.when('/checkout', {
+			controller: 'checkoutController',
+			templateUrl: SETTINGS.views() + 'checkout.html',
 			resolve: {
-				isLoggedIn: checkLogin,
-				exists : checkPage
+				isLoggedIn: checkLogin
+				//exists : checkPage
+			}
+		})
+		.when('/checkout/success', {
+			controller: 'successController',
+			templateUrl: SETTINGS.views() + 'success.html',
+			resolve: {
+				isLoggedIn: checkLogin
+				//exists : checkPage
+			}
+		})
+		.when('/invoice', {
+			controller: 'invoiceController',
+			templateUrl: SETTINGS.views() + 'invoice.html',
+			resolve: {
+				isLoggedIn: checkLogin
+				//exists : checkPage
 			}
 		})
 		.when('/:category*/p/:product', {
@@ -38,17 +54,26 @@ app.config(function($routeProvider, $locationProvider){
 			templateUrl: SETTINGS.views() + 'product.html',
 			resolve: {
 				isLoggedIn: checkLogin,
-				exists : checkPage
+				exists : function(check) {return check.category()}
 			}
 		})
-		.when('/:page/:subpage', {
-			controller: 'pageController',
-			templateUrl: SETTINGS.views() + 'page.html',
+		.when('/:category*', {
+			controller: 'categoryController',
+			templateUrl: SETTINGS.views() + 'category.html',
 			resolve: {
 				isLoggedIn: checkLogin,
-				exists : checkPage
+				exists : function(check) {return check.category()}
 			}
 		})
+		
+		// .when('/:page/:subpage', {
+		// 	controller: 'pageController',
+		// 	templateUrl: SETTINGS.views() + 'page.html',
+		// 	resolve: {
+		// 		isLoggedIn: checkLogin,
+		// 		exists : function(check) {return check.category()}
+		// 	}
+		// })
 		.when('/404', {
 			controller: 'homeController',
 			templateUrl: SETTINGS.views() + 'home.html',
@@ -70,9 +95,33 @@ checkLogin = function() {
 	}
 }
 
-checkPage = function($location, $routeParams) {
-	console.log($location.path())
+app.factory('check', function($location, $routeParams, api) {
+	return {
+		category : function(path) {
+			console.log($location.path())
+			api.post("checkpage", path).then(function(response) {
+				console.log(response);
+			});
+		},
+		product : function(path) {
+			api.post("checkpage", path).then(function(response) {
+				console.log(response);
+			})
+		}
+	}
 	// if ($routeParams.page != "login" || $routeParams.page != "404") {
 	// 	$location.path("/404");
 	// }
-}
+})
+
+app.run(function($rootScope, $location) {
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      if (!USER.admin) {
+        // no logged user, redirect to /login
+        if ( next.templateUrl === "partials/login.html") {
+        } else {
+          $location.path("/login");
+        }
+      }
+    });
+  });
