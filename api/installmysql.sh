d76de997f605084eadda3a88e1e537da8fb5ec37
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-02-06 11:20:30 +0100

    Quite difficult to install python mysql connector.
    A short-hand to ease up the pain. For bash and pip.

---
+++ api/installmysql.sh
@@ -0,0 +1,2 @@
+sudo pip install --allow-external mysql-connector-python mysql-connector-python
+env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install mysql-python 
