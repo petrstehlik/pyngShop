2fc3c0a3d8adb2dab0bc2d6d7087ca15c4deb6e6
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-02-06 11:27:08 +0100

    ADD: config sample and conflict-free gitignore

---
+++ api/config-sample.py
@@ -0,0 +1,17 @@
+CONFIG = {
+	'db' : {
+		'user' : '',
+		'database' : '',
+		'password' : '',
+		'port' : 8889,
+	},
+	'email' : {
+		'username' 	: '',
+		'password'	: '',
+		'server'	: '',
+		'from'		: ''
+	},
+	#'unix_socket' : '/Applications/MAMP/tmp/mysql/mysql.sock'
+	'debug' : False,
+	'version' : '/v1'
+}
