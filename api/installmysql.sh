3a3accc5c534e9c78db3d5bc53564f77f3453586
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-02-27 12:25:25 +0100

    Remove old API

--- api/installmysql.sh
+++
@@ -1,2 +0,0 @@
-sudo pip install --allow-external mysql-connector-python mysql-connector-python
-env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install mysql-python 
