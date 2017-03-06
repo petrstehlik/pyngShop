c55e198720ccc8fd95340a0d0fe62f549e119ef7
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-03-06 09:48:10 +0100

    Moved front-end to www folder

---
+++ www/js/config-sample.js
@@ -0,0 +1,21 @@
+var app = angular.module('pyngShop', ['ngRoute', 'angular.filter', 'ngStorage', 'xeditable']);
+
+var SETTINGS = {
+	template : 'default',
+	templatePath : function() {return ('templates/' + this.template)},
+	views : function() { return(this.templatePath() + '/views/')},
+	partials : function() { return(this.templatePath() + '/partials/')},
+	name : 'pyngShop'
+};
+
+var USER = {
+	user : false,
+	editor : false,
+	admin : false
+}
+
+var API = {
+	host : "localhost",
+	port : "5000",
+	version : "v1"
+}
---
+++ www/js/controllers/ordersController.js
@@ -0,0 +1,83 @@
+app.controller('ordersController', function($scope, auth, api) {
+	$scope.hello = "hello";
+
+	api.get("orders").then(function(r) {
+		$scope.orders = r;		
+	})
+
+	api.get("warehouse").then(function(r) {
+		$scope.products = r;
+	})
+
+	$scope.deleteProduct = function(product_id) {
+		console.log("Deleting product with id: " + product_id)
+		// api.delete("product", {"product_id" : product_id}).then(function(r) {
+		// 	console.log(r)
+		// 	api.get("warehouse").then(function(r) {
+		// 		$scope.products = r;
+		// 	})
+		// })
+	}
+
+	$scope.toggleMan = 0;
+
+	$scope.submitMan = function(man) {
+		console.log(man);
+		api.post("warehouse", man).then(function(r) {
+			console.log(r)
+			api.get("warehouse").then(function(r) {
+				$scope.products = r;
+				$scope.toggleMan = 0;
+			})
+		});
+	}
+
+	$scope.removeMan = function(man) {
+		console.log(man)
+		api.delete('warehouse', {manufacturer_id : man}).then(function(r) {
+			api.get("warehouse").then(function(r) {
+				$scope.products = r;
+			})
+		})
+	}
+
+	api.get("admins").then(function(r) {
+		$scope.admins = r;
+	})
+	$scope.submitAdmin = function(admin) {
+		console.log(admin);
+		api.post("admins", admin).then(function(r){
+			api.get("admins").then(function(r) {
+				$scope.admins = r;
+			})
+		})
+	}
+
+	$scope.deleteAdmin = function(admin) {
+		api.delete('admins', admin).then(function(r) {
+			api.get("admins").then(function(r) {
+				$scope.admins = r;
+			})
+		})
+	}
+})
+
+app.controller('orderDetailController', function($scope, $route, $routeParams, auth, api){
+	console.log($routeParams)
+	api.get("orders/" + $routeParams.order_id).then(function (r) {
+		$scope.invoice = r;
+	})
+
+	$scope.changeStatus = function(status) {
+		var data = {
+			status : status,
+			order_id : $scope.invoice.products[0].order_id 
+		}
+
+		api.post("updateorder", data).then(function(r) {
+			$route.reload()
+		})
+
+		console.log(data)
+	}
+});
