b1b3a214d6cdbbec2c2a081dc03eac89f8be02ee
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-23 20:17:11 +0200

    Move www to www-old
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

--- www/js/services/loginService.js
+++
@@ -1,7 +0,0 @@
-app.service('theme', function() {
-      var themePath = SETTINGS.templatePath();
-      return {
-        path : function() { console.log(themePath); return themePath; }
-        //setTitle: function(newTitle) { title = newTitle; }
-      };
-    });
