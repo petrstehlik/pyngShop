4e059482593374f0793fc32677c48b64010c965c
Matej Vido <xvidom00@stud.fit.vutbr.cz>
2017-04-28 19:56:08 +0200

    api: add customer authorization

--- api/session.py
+++ api/session.py
@@ -2,7 +2,6 @@ from uuid import uuid1 as uuid
 from datetime import datetime, timedelta
 
 from .error import ApiException
-from .role import Role
 
 class SessionException(ApiException):
 	status_code = 401
@@ -13,7 +12,6 @@ class SessionManager(object):
 		self.max_user_sessions = max_user_sessions
 		self.sessions = {}
 		self.user_sessions = {}
-		self.customer_sessions = {}
 
 	@classmethod
 	def from_object(self, config):
@@ -48,28 +46,13 @@ class SessionManager(object):
 			raise SessionException("Session \"" + session_id +
 					"\" already exists")
 
-		if user.role == Role.customer:
-			""" This is customer, store as customer session """
-			if user.id in self.customer_sessions:
-				if len(self.customer_sessions[user.id]) > self.max_user_sessions:
-					raise SessionException("Reached maximum of allowed sessions "
-							"per customer")
-				self.customer_sessions[user.id].append(session_id)
-			else:
-				self.customer_sessions[user.id] = [session_id]
-		elif (user.role == Role.admin or
-				user.role == Role.user or
-				user.role == Role.guest):
-			""" This is user, store as user session """
-			if user.id in self.user_sessions:
-				if len(self.user_sessions[user.id]) > self.max_user_sessions:
-					raise SessionException("Reached maximum of allowed sessions "
-							"per user")
-				self.user_sessions[user.id].append(session_id)
-			else:
-				self.user_sessions[user.id] = [session_id]
+		if user.id in self.user_sessions:
+			if len(self.user_sessions[user.id]) > self.max_user_sessions:
+				raise SessionException("Reached maximum of allowed sessions "
+						"per user")
+			self.user_sessions[user.id].append(session_id)
 		else:
-			raise SessionException("Unsupported role")
+			self.user_sessions[user.id] = [session_id]
 
 		self.sessions[session_id] = {
 			"user" : user,
@@ -86,13 +69,7 @@ class SessionManager(object):
 			raise SessionException("Missing session", payload=session_id)
 
 	def __delete(self, session_id):
-		role = self.sessions[session_id]['user'].role
-		if role == Role.customer:
-			self.customer_sessions[self.sessions[session_id]['user'].id].remove(session_id)
-		elif role == Role.admin or role == Role.user or role == Role.guest:
-			self.user_sessions[self.sessions[session_id]['user'].id].remove(session_id)
-		else:
-			raise SessionException("Unsupported role")
+		self.user_sessions[self.sessions[session_id]['user'].id].remove(session_id)
 		del self.sessions[session_id]
 
 	def __update_expire_time(self, session):
