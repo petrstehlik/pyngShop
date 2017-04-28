430367464d8b3ee767dfb15cc282960ac6ee32da
Matej Vido <vido@cesnet.cz>
2017-04-28 14:43:04 +0200

    api/models: add class Customer

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 from .user import User
 from .models import (Customer, Shipping, Order,
 		Product, Review, OrderedProduct,
 		ProductProperty, TypeProperty,
 		Manufacturer, Category)
+=======
+from .customer import Customer
+from .user import User
+>>>>>>> 8980372... api/models: add class Customer
