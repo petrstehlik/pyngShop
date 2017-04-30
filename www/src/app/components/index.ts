a0216af2b903f4077d06e8d4714f4ff8b448dc29
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-30 23:32:51 +0200

    Add basic components

---
+++ www/src/app/components/index.ts
@@ -0,0 +1,5 @@
+export * from './null/null.component';
+export * from './home/home.component';
+export * from './login/login.component';
+export * from './logout/logout.component';
+export * from './setup/setup.component';
