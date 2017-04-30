cc79ac460653af3613234e0400b8aae3e6100218
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-30 23:34:56 +0200

    Add environments

--- www/src/environments/environment.ts
+++ www/src/environments/environment.ts
@@ -1,7 +1,6 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
-<<<<<<< HEAD
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
@@ -58,10 +57,4 @@ export const environment = {
   }
 };
 
-=======
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
-export const environment = {
-  production: false
-};
->>>>>>> 10e2375... Angular 4 front-end
+// The list of which env maps to which file can be found in `angular-cli.json`.
