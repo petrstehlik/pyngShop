e8549a72abedcc339234c7fa752e319cfcf8ef79
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-30 23:34:44 +0200

    Add base for app

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -1,20 +1,81 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, Request, XHRBackend, RequestOptions} from '@angular/http';
+import { RouterModule, Routes, Router } from '@angular/router';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/';
+import { LoginComponent } from './components/';
+import { LogoutComponent } from './components/';
+import { SetupComponent } from './components/';
+import { NullComponent } from './components/';
+import { TopBarComponent } from './components/topbar/topbar.component';
+
+import { AuthGuard } from './utils/index';
+import { HttpInterceptor } from './utils/index';
+import { SafePipe, SafePipeModule } from 'app/utils/safe.pipe';
+
+import { modules } from './modules';
+
+export const appRoutes: Routes = [
+	{
+		path : 'login',
+		component : LoginComponent
+	},
+	{
+		path : 'logout',
+		component : LogoutComponent,
+		canActivate : [AuthGuard]
+	},
+	{
+		path: '',
+		component: HomeComponent,
+		canActivate : []
+	},
+	{
+		path: '**',
+		component: NullComponent
+	}
+];
+
+export function setFactory (xhrBackend: XHRBackend,
+				requestOptions: RequestOptions,
+				router: Router) {
+	return new HttpInterceptor(xhrBackend, requestOptions, router);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    SetupComponent,
+    NullComponent,
+    TopBarComponent
   ],
   imports: [
+	modules,
+	SafePipeModule,
     BrowserModule,
-    FormsModule,
-    HttpModule
+	FormsModule,
+	HttpModule,
+	NgbModule.forRoot(),
+	RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+	AuthGuard,
+	SafePipe,
+		{
+			provide : Http,
+			useFactory: setFactory,
+			deps: [XHRBackend, RequestOptions, Router]
+		}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
