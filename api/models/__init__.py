01c2a9cc3a418e0da8c77aad64a2b1ccd89f8f5d
Matej Vido <xvidom00@stud.fit.vutbr.cz>
2017-04-30 14:23:44 +0200

    api/models: add ordered products mapping

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,4 +1,5 @@
 from .user import User
 from .models import (Customer, Shipping, Order,
-		Product, Review, ProductProperty,
-		TypeProperty, Manufacturer)
+		Product, Review, OrderedProduct,
+		ProductProperty, TypeProperty,
+		Manufacturer, Category)
