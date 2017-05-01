da75a28f64e6a357ddd96610648f9250b18d0db7
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-01 17:49:36 +0200

    Service: operations with cart

---
+++ www/src/app/services/cart.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+/**
+  * Operations with cart
+  *
+  * \note Only within local storage.
+  */
+@Injectable()
+export class CartService {
+
+    cart : Array<Object> = [];
+
+    constructor() {
+        this.cart = JSON.parse(localStorage.getItem("cart"));
+
+        if (this.cart == null) {
+            this.cart = [];
+        }
+    }
+
+    clear() {
+        localStorage.removeItem("cart");
+        this.cart = [];
+    }
+
+    save() {
+        localStorage.setItem("cart", JSON.stringify(this.cart));
+    }
+
+    length() {
+        return this.cart.length;
+    }
+
+    addItem(item : Object) {
+        for (let product of this.cart) {
+            if (product["product"]["id"] == item["id"]) {
+                product["quantity"] += item["quantity"];
+                this.save();
+                return;
+            }
+        }
+
+        this.cart.push(item);
+
+        this.save();
+    }
+
+    removeItem(item : Object) {
+        for (let product of this.cart) {
+            if (product["product"]["id"] == item["id"]) {
+                product["quantity"] -= item["quantity"];
+                this.save();
+                return;
+            }
+        }
+
+        console.debug("Item not found in cart");
+    }
+}
