872954a0538a05033f7edc363ca8643de636fc70
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-01 20:30:45 +0200

    Fix of not refreshing info about user

--- www/src/app/components/topbar/topbar.component.ts
+++ www/src/app/components/topbar/topbar.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'app/services/user.service';
-import { CartService } from 'app/services/cart.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UserService, CartService, AuthService } from 'app/services';
 
 @Component({
   selector: 'topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss'],
-  providers : [UserService, CartService]
+  providers : [UserService, CartService, AuthService]
 })
 export class TopBarComponent implements OnInit {
 
     constructor(private user : UserService,
-               private cart : CartService) { }
+                private cart : CartService,
+                private auth : AuthService,
+                private route : ActivatedRoute,
+                private router : Router) { }
 
     ngOnInit() {
+
+        console.log(this.route.snapshot.url.toString())
+    }
+
+    logout() {
+        this.auth.logout()
+            .subscribe(
+                data => {
+                    console.log('Success logging out.');
+                    localStorage.removeItem("currentUser");
+                    this.user.refresh();
+                    this.router.navigate(["/"]);
+                },
+                error => {
+                    console.log('Error logging out.');
+                    localStorage.removeItem("currentUser");
+                    //this.router.navigate([this.returnUrl]);
+                }
+            );
     }
 
 }
