f0367d56b804a83f0e0670b0cdda40dc7cb08586
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-01 20:27:37 +0200

    Service: category handling

---
+++ www/src/app/services/category.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+
+@Injectable()
+export class CategoryService {
+
+    constructor(private http : Http) {
+    }
+
+    fetchAll() {
+        return this.http.get('/categories')
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    fetch(id : Number) {
+        return this.http.get('/categories/' + Number(id))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    delete(id : Number) {
+        return this.http.delete('/categories/' + Number(id))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    add(item : Object) {
+        return this.http.post('/categories', JSON.stringify(item))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    update(item : Object) {
+        return this.http.put('/categories/' + Number(item["id"]), JSON.stringify(item))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    private handleError(err : Response | any) {
+        console.log(err);
+        return Promise.reject(err);
+    }
+}
