ad15d7b79a3b93d7b8decbb7a7488c22a480e9d9
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-01 17:45:55 +0200

    Component: Topbar
    Shows logo and user related actions
    
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -11,104 +11,23 @@ import { HomeComponent } from './components/';
 import { LoginComponent } from './components/';
 import { LogoutComponent } from './components/';
 import { SetupComponent } from './components/';
-import { NullComponent } from './components/';
-import { TopBarModule } from './components/topbar/top-bar.module';
-import { CustomerLoginComponent } from './components/customer-login/login.component';
-
+import { NullComponent, TopBarComponent } from './components/';
 
 import { AuthGuard } from './utils/index';
 import { HttpInterceptor } from './utils/index';
 import { SafePipe, SafePipeModule } from 'app/utils/safe.pipe';
 
 import { modules } from './modules';
-import { CategoryBarComponent } from './components/category-bar/category-bar.component';
-import { CategoryItemComponent } from './components/category-item/category-item.component';
-import { CategoryComponent } from './components/category/category.component';
-
-import {UserService} from './services/user.service';
-import { RegisterComponent } from './components/register/register.component'
-
-import {InlineEditorModule} from 'ng2-inline-editor';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ProductNewComponent } from './components/product-new/product-new.component';
-import { AccountComponent } from './components/account/account.component';
-import { CustomFormsModule } from 'ng2-validation';
-import { AdminComponent } from './components/admin/admin.component';
-import { AdminManufacturerComponent } from './components/admin-manufacturer/admin-manufacturer.component';
-import { AdminShippingComponent } from './components/admin-shipping/admin-shipping.component';
-//import { Manufacturer } from './components/manufacturer/manufacturer.component';
-import { TruncatePipe } from './utils/truncate.pipe';
-import { ProductComponent } from './components/product/product.component';
-import { CartComponent } from './components/cart/cart.component';
-import { TotalPipe } from './utils/total.pipe';
-import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
-import { AdminOrderDetailComponent } from './components/admin-order-detail/admin-order-detail.component';
 
 export const appRoutes: Routes = [
 	{
-		path : 'admin/login',
-		component : LoginComponent
-	},
-	{
-		path : 'admin/manufacturers',
-		component : AdminManufacturerComponent
-	},
-	{
-		path : 'admin/shipping',
-		component : AdminShippingComponent
-	},
-	{
-		path : 'admin/orders',
-		component : AdminOrdersComponent
-	},
-	{
-		path : 'admin/orders/:id',
-		component : AdminOrderDetailComponent
-	},
-	{
 		path : 'login',
-		component : CustomerLoginComponent
+		component : LoginComponent
 	},
 	{
 		path : 'logout',
 		component : LogoutComponent,
 		canActivate : [AuthGuard]
-	},
-	{
-		path : 'register',
-		component : RegisterComponent
-	},
-	{
-		path : 'account',
-		component : AccountComponent
-	},
-    {
-        path : 'category/:id',
-        component : CategoryComponent
-    },
-	{
-		path : 'category/:categoryid/product/new',
-		component : ProductNewComponent,
-		canActivate : [AuthGuard],
-		data : {
-			role: 0
-		}
-	},
-	{
-		path : 'product/:id',
-		component : ProductComponent
-	},
-	{
-		path: 'admin',
-		component : AdminComponent,
-		canActivate : [AuthGuard],
-		data : {
-			role : 0
-		}
-	},
-	{
-		path : 'cart',
-		component : CartComponent
 	},
 	{
 		path: '',
@@ -132,43 +51,22 @@ export function setFactory (xhrBackend: XHRBackend,
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CustomerLoginComponent,
     LogoutComponent,
     SetupComponent,
     NullComponent,
-    CategoryBarComponent,
-    CategoryItemComponent,
-    CategoryComponent,
-    ProductCardComponent,
-    ProductNewComponent,
-    RegisterComponent,
-    AccountComponent,
-    AdminComponent,
-    AdminManufacturerComponent,
-    AdminShippingComponent,
-   // Manufacturer,
-    TruncatePipe,
-    ProductComponent,
-    CartComponent,
-    TotalPipe,
-    AdminOrdersComponent,
-    AdminOrderDetailComponent
+    TopBarComponent
   ],
   imports: [
 	modules,
-	TopBarModule,
 	SafePipeModule,
     BrowserModule,
 	FormsModule,
 	HttpModule,
-	InlineEditorModule,
-	CustomFormsModule,
 	NgbModule.forRoot(),
 	RouterModule.forRoot(appRoutes)
   ],
   providers: [
 	AuthGuard,
-	UserService,
 	SafePipe,
 		{
 			provide : Http,
--- www/src/app/components/topbar/topbar.component.ts
+++ www/src/app/components/topbar/topbar.component.ts
@@ -1,57 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { Router, ActivatedRoute } from '@angular/router';
-
-import { UserService, CartService, AuthService } from 'app/services';
-=======
->>>>>>> de2c086... Add basic components
+import { UserService } from 'app/services/user.service';
+import { CartService } from 'app/services/cart.service';
 
 @Component({
   selector: 'topbar',
   templateUrl: './topbar.component.html',
-<<<<<<< HEAD
   styleUrls: ['./topbar.component.scss'],
-  providers : [UserService, CartService, AuthService]
+  providers : [UserService, CartService]
 })
 export class TopBarComponent implements OnInit {
 
     constructor(private user : UserService,
-                private cart : CartService,
-                private auth : AuthService,
-                private route : ActivatedRoute,
-                private router : Router) { }
+               private cart : CartService) { }
 
-    ngOnInit() {}
-
-    logout() {
-        this.auth.logout()
-            .subscribe(
-                data => {
-                    console.log('Success logging out.');
-                    localStorage.removeItem("currentUser");
-                    this.user.refresh();
-                    this.router.navigate(["/"]);
-                },
-                error => {
-                    console.log('Error logging out.');
-                    localStorage.removeItem("currentUser");
-                    //this.router.navigate([this.returnUrl]);
-                }
-            );
+    ngOnInit() {
     }
-=======
-  styleUrls: ['./topbar.component.scss']
-})
-export class TopBarComponent implements OnInit {
-
-	role = {
-		"admin" : true
-	}
-
-  constructor() { }
-
-  ngOnInit() {
-  }
->>>>>>> de2c086... Add basic components
 
 }
