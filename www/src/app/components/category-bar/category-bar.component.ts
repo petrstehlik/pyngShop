801087a366b3c44c690bca61b2f26fa97475908b
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-01 20:31:13 +0200

    CategoryBar fetch categories from server

--- www/src/app/components/category-bar/category-bar.component.ts
+++ www/src/app/components/category-bar/category-bar.component.ts
@@ -1,87 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import {CategoryService } from 'app/services/category.service';
-import {UserService } from 'app/services/user.service';
 
 @Component({
   selector: 'category-bar',
   templateUrl: './category-bar.component.html',
   styleUrls: ['./category-bar.component.scss'],
-  providers : [CategoryService, UserService]
-})
-export class CategoryBarComponent implements OnInit {
-
-    categories : Array<Object>;
-    newcat = "+ Add Category";
-    newsub = "+ Add Subcategory";
-
-    constructor(private category : CategoryService,
-               private user : UserService) { }
-
-    ngOnInit() {
-    	this.load();
-    }
-
-    load() {
-		this.category.fetchAll().subscribe(
-            data => {
-                this.categories = data
-            },
-            error => {
-                console.log(error);
-            }
-        );
-    }
-
-    saveEditable(event : any) {
-        if (event == "+ Add Category") {
-            return;
-        }
-
-        this.category.add({"name" : event}).subscribe(
-			data => {
-				this.load();
-				this.newcat = "+ Add Category";
-			},
-			error => {
-				console.log(error);
-				this.newcat = "+ Add Category";
-			}
-    	);
-
-    }
-
-    newSubCat(event, id : Number) {
-    	if (event == "+ Add Subcategory") {
-			return;
-    	}
-
-    	this.category.add({"name" : event, "parent" : {"id" : id}, "hidden" : false}).subscribe(
-			data => {
-				this.load();
-				this.newsub = "+ Add Subcategory";
-			},
-			error => {
-				console.log(error);
-				this.newsub = "+ Add Subcategory";
-			}
-    	);
-    }
-=======
-=======
->>>>>>> 9107357... Components: Category Bar and Item
-=======
->>>>>>> 9107357... Components: Category Bar and Item
-=======
->>>>>>> 9107357... Components: Category Bar and Item
-@Component({
-  selector: 'category-bar',
-  templateUrl: './category-bar.component.html',
-  styleUrls: ['./category-bar.component.scss']
+  providers : [CategoryService]
 })
 export class CategoryBarComponent implements OnInit {
 
@@ -102,19 +27,17 @@ export class CategoryBarComponent implements OnInit {
         }
     ]
 
-  constructor() { }
+    constructor(private category : CategoryService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.category.fetchAll().subscribe(
+            data => {
+                this.categories = data
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 9107357... Components: Category Bar and Item
-=======
->>>>>>> 9107357... Components: Category Bar and Item
-=======
->>>>>>> 9107357... Components: Category Bar and Item
-=======
->>>>>>> 9107357... Components: Category Bar and Item
 }
