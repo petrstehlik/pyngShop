d08c5a8bbf8104913d2476a33dbe4797490c2979
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-02 15:53:03 +0200

    Add redirection to product and enahnce card look

--- www/src/app/components/product-card/product-card.component.ts
+++ www/src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {TruncatePipe} from 'app/utils/truncate.pipe';
 
 @Component({
   selector: 'product-card',
@@ -9,10 +12,14 @@ export class ProductCardComponent implements OnInit {
 
 	@Input() item : Object;
 
-  constructor() { }
+	constructor(private router : Router) { }
 
 	ngOnInit() {
 		console.log(this.item);
+	}
+
+	redirect() {
+		this.router.navigate(['product', this.item["id"]]);
 	}
 
 }
