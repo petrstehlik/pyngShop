1e42598d9d26c2dd5a63c56830436bf62fd7bd96
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-02 22:27:18 +0200

    Add stats to admin

--- www/src/app/components/admin/admin.component.ts
+++ www/src/app/components/admin/admin.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StatsService } from 'app/services/stats.service';
+import { CategoryService } from 'app/services/category.service';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
+  providers : [StatsService, CategoryService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+	products;
+	orders;
 
-  ngOnInit() {
-  }
+	constructor(private stats : StatsService) { }
+
+	ngOnInit() {
+		this.stats.products().subscribe(
+			data => {
+				this.products = data;
+			},
+			err => {
+				console.log(err);
+			}
+		);
+
+		this.stats.orders().subscribe(
+			data => {
+				this.orders = data;
+			},
+			err => {
+				console.log(err);
+			}
+		);
+
+	}
 
 }
