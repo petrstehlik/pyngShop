6988dff09a1253f14c095b31caf4d1060d113f70
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-02 23:11:24 +0200

    Problems with product listing

--- www/src/app/utils/truncate.pipe.ts
+++ www/src/app/utils/truncate.pipe.ts
@@ -12,6 +12,10 @@ export class TruncatePipe implements PipeTransform {
 		let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
 		let trail = args.length > 1 ? args[1] : '...';
 
-	  	return value.length > limit ? value.substring(0, limit) + trail : value;
+	  	try {
+	  		return value.length > limit ? value.substring(0, limit) + trail : value;
+		} catch (e) {
+			return "";
+		}
 	}
 }
