f13b3d10dc2c648a1dc147b481db099f8764ac77
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-02 21:03:02 +0200

    Finalize ordering process

---
+++ www/src/app/services/order.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+@Injectable()
+export class OrderService {
+
+  constructor(private http : Http) {}
+
+  fetchAll() {
+        return this.http.get('/orders')
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    fetch(id : Number) {
+        return this.http.get('/orders/' + Number(id))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    add(item : Object) {
+        return this.http.post('/orders', JSON.stringify(item))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    update(item : Object) {
+        return this.http.put('/orders/' + Number(item["id"]), JSON.stringify(item))
+            .map((r : Response) => {
+                let response = r.json();
+
+                return response;
+            })
+            .catch(this.handleError);
+    }
+
+    private handleError(err : Response | any) {
+        console.log(err);
+        return Promise.reject(err);
+    }
+}
