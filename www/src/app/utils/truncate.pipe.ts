844023f146940c035fc88114f401299e1e7e8192
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-03 17:50:41 +0200

    Fix truncate pipe

--- www/src/app/utils/truncate.pipe.ts
+++ www/src/app/utils/truncate.pipe.ts
@@ -8,9 +8,14 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'truncate'
 })
 export class TruncatePipe implements PipeTransform {
-	transform(value: string, args: string[]) : string {
-		let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
-		let trail = args.length > 1 ? args[1] : '...';
+	transform(value: string, args: string) : string {
+		console.log(value);
+		console.log("args", args);
+		let limit = parseInt(args, 10);
+		let trail = args.length > 1 ? args : '...';
+
+		console.log("limit");
+		console.log(parseInt(args, 10));
 
 	  	try {
 	  		return value.length > limit ? value.substring(0, limit) + trail : value;
