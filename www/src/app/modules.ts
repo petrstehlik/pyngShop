f458ac64b23382bdaf9f3098e7d108e42ca7245a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-04 09:31:15 +0200

    Remove empty modules

--- www/src/app/modules.ts
+++ www/src/app/modules.ts
@@ -1,11 +1,6 @@
 //import { dummyModule } from 'modules/dummy/dummy.module';
 import { usersModule } from './modules/users/users.module';
 
-import { CategoryModule } from './modules/category/category.module';
-import { ProductModule } from './modules/product/product.module';
-
 export const modules : Array<Object> = [
-	CategoryModule,
-	ProductModule,
 	usersModule
 ]
