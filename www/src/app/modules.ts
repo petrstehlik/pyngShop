4e71923105f210875b5b67890759eb4cca3a2b1d
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-04 09:31:15 +0200

    Remove empty modules

--- www/src/app/modules.ts
+++ www/src/app/modules.ts
@@ -1,15 +1,6 @@
 //import { dummyModule } from 'modules/dummy/dummy.module';
 import { usersModule } from './modules/users/users.module';
 
-<<<<<<< HEAD
 export const modules : Array<Object> = [
-=======
-import { CategoryModule } from './modules/category/category.module';
-import { ProductModule } from './modules/product/product.module';
-
-export const modules : Array<Object> = [
-	CategoryModule,
-	ProductModule,
->>>>>>> e0bddf3... Add base for modules
 	usersModule
 ]
