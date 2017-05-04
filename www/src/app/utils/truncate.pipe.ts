5204d0d6b024a06d8440cff953a995700054494d
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-04 10:00:29 +0200

    Remove debug info from truncate pipe

--- www/src/app/utils/truncate.pipe.ts
+++ www/src/app/utils/truncate.pipe.ts
@@ -9,13 +9,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
 	transform(value: string, args: string) : string {
-		console.log(value);
-		console.log("args", args);
 		let limit = parseInt(args, 10);
 		let trail = args.length > 1 ? args : '...';
-
-		console.log("limit");
-		console.log(parseInt(args, 10));
 
 	  	try {
 	  		return value.length > limit ? value.substring(0, limit) + trail : value;
