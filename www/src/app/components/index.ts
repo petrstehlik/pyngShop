e256298bcbbb780e322f7fdde0d508798313a466
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-04 09:33:43 +0200

    Move topbar and category bar up

--- www/src/app/components/index.ts
+++ www/src/app/components/index.ts
@@ -1,12 +1,7 @@
 export * from './null/null.component';
-<<<<<<< HEAD
 export * from './topbar/topbar.component';
 export * from './category-bar/category-bar.component';
-=======
->>>>>>> de2c086... Add basic components
 export * from './home/home.component';
 export * from './login/login.component';
 export * from './logout/logout.component';
 export * from './setup/setup.component';
-export * from './topbar/topbar.component';
-export * from './category-bar/category-bar.component';
